855cac0e8e7d5f033b1a070326335959f45b1ee9
Merzlikin-Matvey <merzlikinmatvey01@gmail.com>
2023-11-20 20:07:33 +0300

    небольшие изменения в js и html

--- site/js/generateBoard.js
+++ site/js/generateBoard.js
@@ -1,66 +1,72 @@
 const FIGURES_TO_NUMBERS = {
-    '0': 'pawn',
-    '1': 'rook',
-    '2': 'bishop',
-    '3': 'knight',
-    '4': 'queen',
-    '5': 'king'
-}
+  0: "pawn",
+  1: "rook",
+  2: "bishop",
+  3: "knight",
+  4: "queen",
+  5: "king",
+};
 
 const COLORS = {
-    'w': 'white',
-    'b': 'black'
-}
+  w: "white",
+  b: "black",
+};
 
-
+// Создаем доску, просто проходя циклами
 function generateChessboard() {
-    const chessboard = document.getElementById('chessboard');
-    const colors = ['white', 'black']
+  const chessboard = document.getElementById("chessboard");
+  const colors = ["white", "black"];
 
-    for (let row = 0; row < 8; row++){
-        const rowElement = document.createElement('tr');
-        for (let col = 0; col < 8; col++){
-            const cellElement = document.createElement('td');
-            cellElement.className = colors[(row + col) % 2];
-            cellElement.id = String(row + 1) + '_' + String(col + 1)
+  for (let row = 0; row < 8; row++) {
+    const rowElement = document.createElement("tr");
+    for (let col = 0; col < 8; col++) {
+      const cellElement = document.createElement("td");
 
-            cellElement.addEventListener("click", function () {
-                clickHandler(String(row + 1) + '_' + String(col + 1));
-            })
+      cellElement.className = colors[(row + col) % 2];
+      cellElement.id = String(row + 1) + "_" + String(col + 1);
 
-            rowElement.appendChild(cellElement);
-        }
-        chessboard.appendChild(rowElement);
+      // Обработчик нажатия на клетку
+      cellElement.addEventListener("click", function () {
+        clickHandler(String(row + 1) + "_" + String(col + 1));
+      });
+
+      rowElement.appendChild(cellElement);
     }
+    chessboard.appendChild(rowElement);
+  }
 }
 
-function setFigures(figures){
-    try{
-        figures.forEach(figure => {
-            let cage = document.getElementById(
-                String(figure[2]) + '_' + String(figure[3]) 
-            )
-            let imgElement = document.createElement('img');
-            imgElement.src = 'site/res/' + figure[1] + '_' + figure[0].toLowerCase() +'.png';
-            imgElement.classList.add('figure-image');
-            cage.appendChild(imgElement);
-    
-        });
-    } catch (error){
-        window.location.href = 'error'
-    }
-    
+// Устанавливаем фигуры на доску
+function setFigures(figures) {
+  try {
+    figures.forEach((figure) => {
+      let cage = document.getElementById(
+        String(figure[2]) + "_" + String(figure[3])
+      );
+      let imgElement = document.createElement("img");
+      imgElement.src =
+        "site/res/" + figure[1] + "_" + figure[0].toLowerCase() + ".png";
 
+      imgElement.classList.add("figure-image");
+      // Привязываем фигуру к клетке
+      cage.appendChild(imgElement);
+    });
+  } catch (error) {
+    // В случае ошибки (чаще всего неверный код), переходим на специальную страницу
+    window.location.href = "error";
+  }
 }
 
-function decrypt(s){
-    let decrypted = [];
-    for (let i = 0; i < s.length; i += 4){
-        decrypted.push([
-            FIGURES_TO_NUMBERS[s[i]], COLORS[s[i + 1]], Number(s[i + 2]), Number(s[i + 3])
-        ])
-    }
-    return decrypted
+// Расшифровываем переданную расстановку фигур
+function decrypt(s) {
+  let decrypted = [];
+  for (let i = 0; i < s.length; i += 4) {
+    decrypted.push([
+      FIGURES_TO_NUMBERS[s[i]],
+      COLORS[s[i + 1]],
+      Number(s[i + 2]),
+      Number(s[i + 3]),
+    ]);
+  }
+  return decrypted;
 }
-
-
--- site/js/movement.js
+++ site/js/movement.js
@@ -1,16 +1,20 @@
+// Обработчик нажатий на клетку
 function clickHandler(id) {
+    // Чаще всего getLastReceivedData() это доступные для атаки клетки 
     let attackPositions = getLastReceivedData()
-    console.log(attackPositions, typeof attackPositions)
     if (attackPositions != null &&
         attackPositions != 'white' && 
         attackPositions != 'black'){
+        // Если это рил доступные клетки для атаки, сверяю, могу ли я сходить куда хочу
         if (attackPositions.some(
                 arr => arr[0] == Number(id[0]) && arr[1] == Number(id[2]))
             ){  
+                // Если могу, отправляю на сервер запрос о ходе и выполняю move()
                 sendMessage('move|' + getSelectedPosition() + '|' + id)
                 .then(() => move())
         }
         else{
+            // В противном случае просто выбираю клетку
             selectPosition(id)
         }
     }
@@ -21,14 +25,15 @@ function clickHandler(id) {
 }
 
 
-
+// Выбор клетки, из которой буду ходить
 function selectPosition(id){
     sendMessage('get_color|' + id)
     .then(() => checkColor(getLastReceivedData(), id))
 }
 
-
+// Проверяю, можно ли сейчас сходить определенным цветом
 function checkColor(color, id){
+    // Делаем вид, что никакой актуальной информации нет
     setLastReceivedData(null)
     if (color == getNowMove()){
         setSelectedPosition(id)
@@ -36,25 +41,29 @@ function checkColor(color, id){
         .then(() => highlighAttackPositions())
         .catch((error) => console.error('Error:', error));
     }
+    // Делаем вид, что никакой актуальной информации нет
     setLastReceivedData(null)
 
 }
 
-
+// Подсвечиваю доступные для атаки клетки
 function highlighAttackPositions(){
     console.log(getLastReceivedData())
     if (getLastReceivedData() != null){
+        // Убираю подсветку старых клеток
         removeHighlighPositions()
         setAttackPositions(getLastReceivedData())
         getLastReceivedData().forEach(array => {
             i = array[0]
             j = array[1]
             cage = document.getElementById(String(i) + '_' + String(j))
+            // Добавляем стиль, который подсветит клетку
             cage.classList.add('attack_position')
         });
     }
 }
 
+// Убрать подсветку всех клеток
 function removeHighlighPositions(){
     for (let i = 1; i < 9; i++){
         for (let j = 1; j < 9; j++){
@@ -65,11 +74,13 @@ function removeHighlighPositions(){
 }
 
 function move(){
+    // x1 y1 - откуда идет; x2 y2 - куда идет
     let x1 = getSelectedPosition()[0]
     let y1 = getSelectedPosition()[2]
     let x2 = getLastReceivedData().split('|')[2].split('_')[0]
     let y2 = getLastReceivedData().split('|')[2].split('_')[1]
 
+    // Убираю фигуру со стартовой клетки
     let oldCage = document.getElementById(x1 + '_' + y1)
     let images = oldCage.getElementsByTagName('img');
 
@@ -77,6 +88,7 @@ function move(){
         oldCage.removeChild(images[0]);
     }
 
+    // Убираю фигуру из целевой клетки. если она там есть
     let newCage = document.getElementById(x2 + '_' + y2)
     images = newCage.getElementsByTagName('img');
 
@@ -84,6 +96,7 @@ function move(){
         newCage.removeChild(images[0]);
     }
 
+    // Устанавливаем нашу фигуру на новую позицию
     let img = document.createElement('img')
     let color = getLastReceivedData().split('|')[0].toLowerCase()
     let name = getLastReceivedData().split('|')[1].toLowerCase()
@@ -92,12 +105,16 @@ function move(){
     img.classList.add('figure-image');
     newCage.appendChild(img)
 
+    // После хода убираем позицию
     removeHighlighPositions()
 
+    // Если мат, то перекидывает на финальную страницу
     if (getLastReceivedData().includes('checkmate')){
         window.location.href = "/win?winner=" + getNowMove()
     }
 
+    // Делаем вид, что никакой актуальной информации нет
     setLastReceivedData(null)
+    // Передаем ход
     changeNowMove()
 }
