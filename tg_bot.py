6c832ad1afe4cda96a5787fcc5bd89a3b7e54c65
Артём Абрамов <temabratem@gmail.com>
2023-12-24 13:40:21 +0300

    я русский бот

--- tg_bot.py
+++ tg_bot.py
@@ -1,60 +1,74 @@
 import json
+
 import logging
-from telegram import (
-    KeyboardButton,
-    ReplyKeyboardMarkup,
-    ReplyKeyboardRemove,
-    Update,
-    WebAppInfo,
-)
-from telegram.ext import (
-    Application,
-    CommandHandler,
-    ContextTypes,
-    MessageHandler,
-    filters,
-)
+
+
+from telegram import KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update, WebAppInfo
+
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+
+# Enable logging
 
 logging.basicConfig(
+
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+
 )
+
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+
 logging.getLogger("httpx").setLevel(logging.WARNING)
+
+
 logger = logging.getLogger(__name__)
 
 
+
+# Define a `/start` command handler.
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+    """Send a message with a button that opens a the web app."""
+
     await update.message.reply_text(
+
+        "Привет! ты в телеграм боте синей команды, нажми на кнопку, чтобы поиграть в самую оригинальную игру",
+
         reply_markup=ReplyKeyboardMarkup.from_button(
+
             KeyboardButton(
-                text="Start the game",
+
+                text="Начать игру",
+
                 web_app=WebAppInfo(url="https://chess.projectalpha.ru/game"),
+
             )
+
         ),
+
     )
 
-
-async def web_app_data(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    data = json.loads(update.effective_message.web_app_data.data)
-    await update.message.reply_html(
-        text=(
-            f"You selected the color with the HEX value <code>{data['hex']}</code>. The "
-            f"corresponding RGB value is <code>{tuple(data['rgb'].values())}</code>."
-        ),
-        reply_markup=ReplyKeyboardRemove(),
-    )
 
 def main() -> None:
-    application = (
-        Application.builder()
-        .token("6658441431:AAHIr7viLVGtylMQSBTN0rlzRt05nLW5m50")
-        .build()
-    )
+
+    """Start the bot."""
+
+    # Create the Application and pass it your bot's token.
+
+    application = Application.builder().token("6658441431:AAHIr7viLVGtylMQSBTN0rlzRt05nLW5m50").build()
+
+
     application.add_handler(CommandHandler("start", start))
-    application.add_handler(
-        MessageHandler(filters.StatusUpdate.WEB_APP_DATA, web_app_data)
-    )
+
+
+
+    # Run the bot until the user presses Ctrl-C
+
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
+
 if __name__ == "__main__":
+
     main()
