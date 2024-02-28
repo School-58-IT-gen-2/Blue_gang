4ed2351f9ecc68ce61cd954898db8bef8daa3827
Step4ansky <stepa081022@gmail.com>
2024-02-28 21:49:23 +0300

    Подключение к базе данных готово

--- site/db.py
+++ site/db.py
@@ -2,7 +2,6 @@ import psycopg2
 class Adapter():
 
     def __init__(self, host, port, sslmode, dbname, user, password, target_session_attrs):
-        
         self.host=host
         self.port=port
         self.sslmode=sslmode
@@ -12,8 +11,7 @@ class Adapter():
         self.target_session_attrs=target_session_attrs
         
     def connect(self):
-        #try:
-        if 1:
+        try:
             self.conn = psycopg2.connect(f"""
                 host={self.host}
                 port={self.port}
@@ -23,12 +21,12 @@ class Adapter():
                 password={self.password}
                 target_session_attrs={self.target_session_attrs}
             """)
-        #except:
-            #print('connection error')
+        except:
+            print('connection error')
+        self.cursor = self.conn.cursor()
 
     def select(self, table):
         request = f'SELECT * FROM "Blue_project"."{table}"'
-        self.cursor = self.conn.cursor()
         self.cursor.execute(request)
         data = self.cursor.fetchall()
         return data
@@ -41,6 +39,10 @@ class Adapter():
     def insert(self, table, collumns, values):
         request_insert = f'INSERT INTO "Blue_project"."{table}" ({collumns}) VALUES ({values})'
         self.cursor.execute(request_insert)
+        self.conn.commit()
 
-db = Adapter('rc1d-9cjee2y71olglqhg.mdb.yandexcloud.net','6432','verify-full','sch58_db','Admin','atdhfkm2024','read-write')
+db = Adapter("rc1d-9cjee2y71olglqhg.mdb.yandexcloud.net","6432","verify-full","sch58_db","Admin","atdhfkm2024","read-write")
 db.connect()
+db.update("users",'"username"=\'werwer1\', "password"=\'1234t\' ,"mail" = \'werwer2@wer\', "rating"=15342, created=333333', '1')
+db.insert("users", 'id, "username", "password", "mail", "rating", "created"', "DEFAULT ,\'werwer2\', \'werwer2\', \'werwer2\' , 123, 1233")
+print(db.select('users'))
