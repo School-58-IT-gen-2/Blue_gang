4ab96628e9ac686624829c5938d81bf5583cc907
Step4ansky <stepa081022@gmail.com>
2024-02-29 20:50:02 +0300

    Реворк адаптера по гайду Александра

--- site/db.py
+++ site/db.py
@@ -1,48 +1,43 @@
 import psycopg2
 class Adapter():
 
-    def __init__(self, host, port, sslmode, dbname, user, password, target_session_attrs):
-        self.host=host
-        self.port=port
-        self.sslmode=sslmode
-        self.dbname=dbname
-        self.user=user
-        self.password=password
-        self.target_session_attrs=target_session_attrs
+    def __init__(self):
+        self.__connect()
         
-    def connect(self):
+    def __connect(self):
         try:
             self.conn = psycopg2.connect(f"""
-                host={self.host}
-                port={self.port}
-                sslmode={self.sslmode}
-                dbname={self.dbname}
-                user={self.user}
-                password={self.password}
-                target_session_attrs={self.target_session_attrs}
+                host=rc1d-9cjee2y71olglqhg.mdb.yandexcloud.net
+                port=6432
+                sslmode=verify-full
+                dbname=sch58_db
+                user=Admin
+                password=atdhfkm2024
+                target_session_attrs=read-write
             """)
         except:
             print('connection error')
-        self.cursor = self.conn.cursor()
-
-    def select(self, table):
-        request = f'SELECT * FROM "Blue_project"."{table}"'
-        self.cursor.execute(request)
-        data = self.cursor.fetchall()
+        
+    def get(self, requeste ,table):
+        request = f'SELECT {requeste} FROM "Blue_project"."{table}"'
+        cursor = self.conn.cursor()
+        cursor.execute(request)
+        data = cursor.fetchall()
         return data
     
     def update(self, table, request, id):
         request_update = f'UPDATE "Blue_project"."{table}" SET {request} WHERE id={id}'
-        self.cursor.execute(request_update)
+        cursor = self.conn.cursor()
+        cursor.execute(request_update)
         self.conn.commit()
 
     def insert(self, table, collumns, values):
         request_insert = f'INSERT INTO "Blue_project"."{table}" ({collumns}) VALUES ({values})'
-        self.cursor.execute(request_insert)
+        cursor = self.conn.cursor()
+        cursor.execute(request_insert)
         self.conn.commit()
 
-db = Adapter("rc1d-9cjee2y71olglqhg.mdb.yandexcloud.net","6432","verify-full","sch58_db","Admin","atdhfkm2024","read-write")
-db.connect()
+db = Adapter()
 db.update("users",'"username"=\'werwer1\', "password"=\'1234t\' ,"mail" = \'werwer2@wer\', "rating"=15342, created=333333', '1')
-db.insert("users", 'id, "username", "password", "mail", "rating", "created"', "DEFAULT ,\'werwer2\', \'werwer2\', \'werwer2\' , 123, 1233")
-print(db.select('users'))
+#db.insert("users", 'id, "username", "password", "mail", "rating", "created"', "DEFAULT ,\'werwer2\', \'werwer2\', \'werwer2\' , 123, 1233")
+print(db.get("*",'users'))
