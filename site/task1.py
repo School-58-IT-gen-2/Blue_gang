035ada8a2141ba042e3351b25a0a43f022e0ad65
Step4ansky <stepa081022@gmail.com>
2024-03-25 20:08:46 +0300

    ой

--- site/task1.py
+++ site/task1.py
@@ -3,9 +3,9 @@
     c."Name", 
     c."Crew", 
     c."Captain", 
-    COALESCE(p."Name", 'unknown') AS "Location"
+    COALESCE(CAST(p."id" AS text), 'unknown') AS "Location"
 FROM "Galactic Empire"."Cruisers" c
-LEFT JOIN "Galactic Empire"."Planets" p ON c."Name" = CAST(p."id" AS text)
+LEFT JOIN "Galactic Empire"."Planets" p ON c."Location" = p."Name"
 WHERE c."Crew" <= 200 AND c."Captain" LIKE 'L%'
 
 
